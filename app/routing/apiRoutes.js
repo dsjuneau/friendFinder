a5c2d57bfd85f29e357ee2059bfc6dcaba13b995
Scott Juneau <scott@junlaw.com>
2019-06-05 18:00:30 -0500

    Everything working except for verification

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,7 +5,22 @@ module.exports = function(app) {
     res.json(allFriends);
   });
   app.post("/api/friends", function(req, res) {
-    let match = allFriends[0];
-    res.json(match);
+    let lowestIndex = 0;
+    let currentScore = Infinity;
+    function calScore(arr1, arr2) {
+      let score = 0;
+      for (let i = 0; i < arr1.length; i++) {
+        score = score + Math.abs(arr1[i] - parseInt(arr2[i]));
+      }
+      return score;
+    }
+    for (let i = 0; i < allFriends.length; i++) {
+      if (calScore(allFriends[i].scores, req.body.scores) < currentScore) {
+        lowestIndex = i;
+        currentScore = calScore(allFriends[i].scores, req.body.scores);
+      }
+    }
+
+    res.json(allFriends[lowestIndex]);
   });
 };
